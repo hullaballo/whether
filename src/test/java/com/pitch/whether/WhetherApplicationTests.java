a6ee327e450d340b696bed0bff5d150c6f3a863d
hullaballo <nmclaughlin27@gmail.com>
2024-10-04 11:29:03 +0100

    First Commit

---
+++ src/test/java/com/pitch/whether/WhetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitch.whether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
